c8b6b7b4f4b62b07ffd09ad50cce47d18c17248b
Huber Gilt Lopez <hgilt@IGPmaster2.igp.gob.pe>
2018-11-10 07:55:39 -0500

    add laboratory

---
+++ laboratorio1/calculadora.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<math.h>
+void main(){
+
+    int operacion=0;
+
+    printf("\t\tBienvenidos a la calculadora\n");
+    printf("\t\t****************************\n");
+    printf("\n");
+    printf("\t\tPrimero escoja una operación\n");
+    printf("\n");
+    printf("\n");
+    printf("\t\t****************************\n");
+    printf("\t\t          MENU              \n");
+    printf("\t\t****************************\n");
+    printf("\t\t* 1) Sumar                 *\n");
+    printf("\t\t****************************\n");
+
+    scanf(operacion)
+}
