19465300925b902c6ece100ad3f6414114f631d1
Huber Gilt Lopez <hgilt@IGPmaster2.igp.gob.pe>
2018-11-14 15:32:16 -0500

    trabajando con bites

---
+++ clase5/correr.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main(){
+    
+    int a=3;
+    printf("%d \n",a);
+    a = a << 1;
+
+    printf("%d \n",a);
+    a = a << 1;
+   
+    printf("%d \n",a);
+    a = a << 1;
+
+    printf("%d \n",a);
+    a = a << 1;
+
+    printf("%d \n",a);
+
+    return 0;
+}
---
+++ clase5/tamano.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+ int numero1;
+ long numero2;
+ long long int numero3; 
+ printf("size of int %d\n", sizeof(int));
+ printf("size of long %d\n", sizeof(long));
+ printf("size of long int %d\n", sizeof(long long));
+ return 0;
+}
